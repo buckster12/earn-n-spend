f573c94da4cab3fae641983272230fb5ca5b24de
buckster12 <roboks@mail.ru>
2019-04-13 19:46:09 +0300

    reconfigured files and added backend

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import NavBar from './NavBar/NavBar';
+import Questions from "./Questions/Questions";
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <NavBar/>
+                <Questions/>
+            </div>
+        );
+    }
+}
+
+export default App;
